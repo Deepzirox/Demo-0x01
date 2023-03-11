084e09efd42e15cd762b724b04986d7ed007b869
jdtp125753 <jhojan1806@gmail.com>
2023-03-11 08:24:55 -0500

    calibrando el salto

--- Assets/Scripts/Charaters/Ninja/Move.cs
+++ Assets/Scripts/Charaters/Ninja/Move.cs
@@ -11,34 +11,66 @@ public class Move : MonoBehaviour
     public float speedCam = 110.5f;
     public float speedMoving = 8.0f;
     public float speedRotation = 90.6f;
-    public float camRotation=0f;
+    public float camRotation = 0f;
     public float x, y, mx, my;
+
+    public Rigidbody rb;
+    public float forceJump = 8.0f;
+    public bool icanJump;
+
+
+    public float posicionY = 1.0f;
     void Start()
     {
-         anim = GetComponent<Animator>();
-        cam = transform.GetChild(0).GetComponent<Transform>(); 
-        control= GetComponent<CharacterController>();
+
+        icanJump = false;
+        
+        anim = GetComponent<Animator>();
+        cam = transform.GetChild(0).GetComponent<Transform>();
+        control = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate(){
+
+        transform.Rotate(new Vector3(0, mx, 0) * speedCam * Time.deltaTime);
+        transform.Rotate(0, x * Time.deltaTime * speedRotation, 0);
+        
+        transform.Translate(0, 0, y* Time.deltaTime * speedMoving);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 posicionActual = transform.position;
+        posicionActual.y = posicionY;
+        transform.position = posicionActual;
+
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-        mx = Input.GetAxis("Mouse X");
-        my = Input.GetAxis("Mouse Y");
-        
-        transform.Rotate(new Vector3(0, mx, 0) * speedCam * Time.deltaTime);
-        transform.Rotate(0, x * Time.deltaTime * speedRotation, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * speedMoving);
-        
+
         anim.SetFloat("X", x);
         anim.SetFloat("Y", y);
-       
-        camRotation -= my * speedCam * Time.deltaTime;
-        camRotation = Mathf.Clamp(camRotation, -90, 90);
-        cam.localRotation = Quaternion.Euler(new Vector3(camRotation, 0, 0));
-      
+
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            icanJump = true;
+            if(icanJump){
+            {
+                anim.SetBool("Jump",true);
+                rb.AddForce(new Vector3(0, forceJump, 0),ForceMode.Impulse);
+            }
+            //anim.SetBool("touchFloor", true);
+            //anim.SetBool("Jump",false);
+        }
+        else{
+            Iamfalling();
+        }
+    }
+    public void Iamfalling()
+    {
+        anim.SetBool("touchFloor", false);
+        anim.SetBool("Jump",false);
     }
 }
